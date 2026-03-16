9e4e8731f2b264b187a084b8a470286bc4db04f8
Star <thatsocialistkitty@gmail.com>
2026-03-16 02:27:26 -0400

    meow :3

--- source/main.zig
+++ source/main.zig
@@ -1,11 +1,11 @@
 const std: type = @import("std");
-const MeowWindow: type = @import("MeowWindow");
+const meowWindow: type = @import("MeowWindow");
 
 pub fn main() !void {
     var debugAllocator: std.heap.DebugAllocator(.{}) = .init;
     defer _ = debugAllocator.deinit();
     
-    var window: *MeowWindow.Window = try .create(
+    var window: *meowWindow.Window = try .create(
         debugAllocator.allocator(),
         "meow :3",
         .{500,325},
@@ -13,8 +13,7 @@ pub fn main() !void {
     );
     defer window.destroy();
     
-    while (window.getRunning()) {
-        window.emitEvents();
-        window.renderFrame();
+    while (window.stepEventLoop()) {
+        // :p
     }
 }
